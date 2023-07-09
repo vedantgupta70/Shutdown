14383e1861e3c01538fccc8e2e1e2015be58fed7
vedantgupta70 <138466451+vedantgupta70@users.noreply.github.com>
2023-07-09 15:51:38 +0530

    Update ShutDownApp.py

--- ShutDownApp.py
+++ ShutDownApp.py
@@ -18,7 +18,6 @@ st.title("ShutDown App")
 st.geometry("500x500")
 st.config(bg="Black")
 
-
 r_button=Button(st, text="Restart",font=("Time New Roman",20,"bold"),relief=RAISED,cursor="arrow",command=restart)
 r_button.place(x=150,y=60,height=50,width=200)
 
@@ -26,17 +25,11 @@ r_button.place(x=150,y=60,height=50,width=200)
 rt_button=Button(st, text="Restart Time",font=("Time New Roman",20,"bold"),relief=RAISED,cursor="arrow",command=restart_time)
 rt_button.place(x=150,y=170,height=50,width=200)
 
-
-
 rl_button=Button(st, text="Log-out",font=("Time New Roman",20,"bold"),relief=RAISED,cursor="arrow",command=logout)
 rl_button.place(x=150,y=270,height=50,width=200)
 
 
 rs_button=Button(st, text="ShutDown",font=("Time New Roman",20,"bold"),relief=RAISED,cursor="arrow",command=shutdown)
 rs_button.place(x=150,y=370,height=50,width=200)
-
-
-
-
 
 st.mainloop()
